0a5b35f463b1875d71ad5847a604acf412b1d6d2
Blessyclaire <akpomudiaruth@gmail.com>
2023-11-18 19:35:36 +0100

    Flask web app

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+
 app = Flask (__name__)
+
 @app.route("/")
 def home():
     return "Hello, I am almost a Devops Engineer!"
 
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0', port=5000)
+
+http_server = WSGIServer(('', 5000), app)
+http_server.serve_forever()    
